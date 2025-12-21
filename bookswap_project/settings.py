4f6b7a2c61791529e5267c413df3e02157259229
Heartwill Gbekle <heartwill@Heartwills-MacBook-Air-6932.local>
2025-12-20 22:40:08 -0500

    Fix middleware settings for admin app

--- bookswap_project/settings.py
+++ bookswap_project/settings.py
@@ -42,8 +42,11 @@ INSTALLED_APPS = [
     'core',
 ]
 
+# settings.py
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
