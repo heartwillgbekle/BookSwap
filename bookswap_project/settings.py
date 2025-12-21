e5327854aa105adaa3d9887b03b9e14c7229c43a
Heartwill Gbekle <heartwill@Heartwills-MacBook-Air-6932.local>
2025-12-20 21:36:37 -0500

    Added production settings and requirements for deployment

--- bookswap_project/settings.py
+++ bookswap_project/settings.py
@@ -136,3 +136,13 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
+
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # Add this right here
+    # ... rest of your middleware
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
