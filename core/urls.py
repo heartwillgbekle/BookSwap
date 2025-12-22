b8efee5f7984251a4809c226757c2dc3e544ec8d
Heartwill Gbekle <heartwill@Heartwills-MacBook-Air-6932.local>
2025-12-22 00:34:40 -0500

    Clean up urls and fix registration NameError

--- core/urls.py
+++ core/urls.py
@@ -1,19 +1,19 @@
 # core/urls.py
-
 from django.urls import path
-# Update this import
-from .views import CreateUserView, BookLookupView, ListingCreateView, ListingListView, RegisterView, ListingDeleteView
-from rest_framework_simplejwt.views import TokenObtainPairView
+from .views import (
+    BookLookupView, 
+    ListingCreateView, 
+    ListingListView, 
+    ListingDeleteView, 
+    RegisterView 
+)
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('listings/delete/<int:pk>/', ListingDeleteView.as_view(), name='listing-delete'),
-    path('register/', CreateUserView.as_view(), name='register'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('books/lookup/', BookLookupView.as_view(), name='book-lookup'),
-    # Add this line for creating listings
-    path('listings/create/', ListingCreateView.as_view(), name='listing-create'),
     path('listings/', ListingListView.as_view(), name='listing-list'),
+    path('listings/create/', ListingCreateView.as_view(), name='listing-create'),
+    path('listings/delete/<int:pk>/', ListingDeleteView.as_view(), name='listing-delete'),
 ]
 
 
