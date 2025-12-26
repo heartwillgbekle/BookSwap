8ee775f4f94bac9b2bb3a2e9f84fa1fc3c8a5f86
Heartwill Gbekle <heartwill@Heartwills-MacBook-Air-6932.local>
2025-12-26 18:31:29 -0500

    Fix URL routing to remove double API prefix

--- bookswap_project/urls.py
+++ bookswap_project/urls.py
@@ -1,36 +1,13 @@
-"""
-URL configuration for bookswap_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
+# bookswap_project/urls.py
 
 from django.contrib import admin
 from django.urls import path, include
-from core.views import HomeView 
-from rest_framework.authtoken.views import obtain_auth_token
-from django.http import HttpResponse
-
-def home_view(request):
-    return HttpResponse("<h1>Welcome to the BookSwap API</h1><p>The server is running!</p>")
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
+    # Admin Panel
     path('admin/', admin.site.urls),
-    path('api/login/', obtain_auth_token),
-    path('api/', include('core.urls')),
+    
+    # Send EVERYTHING else to core/urls.py
+    # We use empty string '' so it doesn't add any extra prefixes
+    path('', include('core.urls')), 
 ]
-
-
-
