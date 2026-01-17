10772074225d1557f45e13e739570976f075897a
Heartwill Gbekle <heartwill@Heartwills-MacBook-Air-7814.local>
2026-01-17 01:15:13 -0500

    Add required Allauth middleware

--- bookswap_project/settings.py
+++ bookswap_project/settings.py
@@ -154,6 +154,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
