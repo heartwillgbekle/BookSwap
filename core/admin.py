35a0f7a1681020e743f2711511faffd49c2a0cbb
Heartwill Gbekle <heartwill@Heartwills-MacBook-Air-7814.local>
2026-01-16 23:07:40 -0500

    Update models with ISBN cover logic and add rest_framework to requirements

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+# core/admin.py
+from django.contrib import admin
+from .models import Listing
+
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'price', 'category') # These columns show in the list
